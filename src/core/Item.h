7ed060daa88410ae98668ed38feb8091430d4712
Richard Huveneers <richard@huveneers.com>
2024-12-29 22:01:27 +0100

    Added missing include

--- src/core/Item.h
+++ src/core/Item.h
@@ -89,13 +89,13 @@ public:
     BASEUUID = 0x41,              // Base UUID of Alias or Shortcut (v4)
     ALIASUUID = 0x42,             // UUID indicates this is an Alias (v4)
     SHORTCUTUUID = 0x43,          // UUID indicates this is a Shortcut (v4)
-    DATA_ATT_TITLE = 0x44,
-    DATA_ATT_MEDIATYPE = 0x45,
-    DATA_ATT_FILENAME = 0x46,
-    DATA_ATT_FILECTIME = 0x47,    // Add comments..
+    DATA_ATT_TITLE = 0x44,        // User-friendly name for the attachment
+    DATA_ATT_MEDIATYPE = 0x45,    // MIME type - text string describing Media Type
+    DATA_ATT_FILENAME = 0x46,     // Original file name
+    DATA_ATT_FILECTIME = 0x47,    // Times associated with the attachment file at the time of attachment
     DATA_ATT_FILEMTIME = 0x48,
     DATA_ATT_FILEATIME = 0x49,
-    DATA_ATT_CONTENT = 0x4a,
+    DATA_ATT_CONTENT = 0x4a,      // The binary representation of the attachment
     LAST_DATA,                    // Start of unknown fields!
     LAST_ITEM_DATA_FIELD = 0x5f,  // beyond this is for other CItem subclasses
 
